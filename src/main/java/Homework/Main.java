cdab3de4fc0486c8e792d1643b0bcdcb9479608f
Sierra Buchanan <Sierrabuchanan97@gmail.com>
2021-09-07 19:16:58 -0400

    first commit

---
+++ src/main/java/Homework/Main.java
@@ -0,0 +1,115 @@
+package Homework;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+
+            //Homework for September 7th
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is what i've learned");
+
+        String nickname = "Cece";
+        System.out.println(nickname);
+
+        int myNum = 45;
+        System.out.println(myNum);
+
+        float aah = 6.7f;
+        System.out.println(aah);
+
+        char hehe = 'Y';
+        System.out.println(hehe);
+
+        boolean gang = false;
+        System.out.println(gang);
+
+        byte coolin = 120;
+        System.out.println(coolin);
+
+        short product = 6500;
+        System.out.println(product);
+
+        int numba = 20000;
+        System.out.println(numba);
+
+        long number = 19000000000L;
+        System.out.println(number);
+
+        float num = 7.88f;
+        System.out.println(num);
+
+        double mynum = 20.98d;
+        System.out.println(mynum);
+
+        float on = 32e5f;
+        double off = 54E8d;
+        System.out.println(on);
+        System.out.println(off);
+
+        boolean Javacoo = true;
+        boolean Javaite = false;
+        System.out.println(Javacoo);
+        System.out.println(Javaite);
+
+        char s = 73, g = 89, k = 41;
+        System.out.println('s');
+        System.out.println('g');
+        System.out.println('k');
+
+        int inz = 32;
+        double douz = inz;
+        System.out.println(inz);
+        System.out.println(douz);
+
+        double trouble = 51d;
+        int wcw = (int) trouble;
+        System.out.println(trouble);
+        System.out.println(wcw);
+
+        int z = 200 + 93;
+        System.out.println(z);
+
+        int dum1 =  200 + 93;
+        int dum2 = dum1 + 234;
+        int dum3 = dum2 + dum2;
+        System.out.println(dum1);
+        System.out.println(dum1 +234);
+        System.out.println(dum2 + dum2);
+
+        String tct = "I hope I did the homework right";
+        System.out.println(tct.toUpperCase());
+        System.out.println(tct.toLowerCase());
+
+        String out = "Perfect Score 'here'";
+        System.out.println(out.indexOf("here"));
+
+        String myDog = "Doggie";
+        String herDog = "Her";
+        System.out.println(herDog + " " + myDog);
+
+        String life = "The Beginning";
+        String Pass = "Of the End";
+        System.out.println(life.concat(Pass));
+
+        String txt = "We are the \"champions\" now.";
+        System.out.println(txt);
+
+        String tca = "I don't \\ know yet.";
+        System.out.println(tca);
+
+        System.out.println(Math.max(5, 12));
+        System.out.println(Math.min(6, 13));
+        System.out.println(Math.sqrt(72));
+        System.out.println(Math.abs(-3.9));
+        System.out.println(Math.random());
+        int randomsht = (int)(Math.random()* 200);
+        System.out.println(randomsht);
+
+
+
+    }
+
+}
+
+
+
